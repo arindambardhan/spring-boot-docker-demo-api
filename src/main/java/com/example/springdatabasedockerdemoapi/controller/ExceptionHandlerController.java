bbcb74b26911bd337e57ea6e5893952627d8ad3a
Arindam <arindambardhan2196@gmail.com>
2022-09-28 21:55:30 +0530

    made few changes

--- src/main/java/com/example/springdatabasedockerdemoapi/controller/ExceptionHandlerController.java
+++ src/main/java/com/example/springdatabasedockerdemoapi/controller/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.example.springdatabasedockerdemoapi.controller;
 
+import com.example.springdatabasedockerdemoapi.exceptions.NoRecordFoundException;
 import com.example.springdatabasedockerdemoapi.exceptions.StudentNotFoundException;
 import com.example.springdatabasedockerdemoapi.model.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -8,14 +9,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(value = "StudentController")
+@RestControllerAdvice
 @Slf4j
 public class ExceptionHandlerController {
 
     @ExceptionHandler(StudentNotFoundException.class)
-    public ResponseEntity handleError(Exception exception) {
+    public ResponseEntity handlesError_1(Exception exception) {
         log.error(exception.getMessage());
-        Response response = Response.builder().code(HttpStatus.BAD_REQUEST).message(exception.getMessage()).build();
+        Response response = Response.builder().code(HttpStatus.BAD_REQUEST.value()).message(exception.getMessage()).build();
         return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(NoRecordFoundException.class)
+    public ResponseEntity handlesError_2(Exception exception) {
+        log.error(exception.getMessage());
+        Response response = Response.builder().code(HttpStatus.ACCEPTED.value()).message(exception.getMessage()).build();
+        return ResponseEntity.accepted().body(response);
     }
 }
